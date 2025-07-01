a4f933aabed87dae23c18b2794d43abd4b4360fa
Matthew McTighe <matthewmctighe1@gmail.com>
2025-07-01 14:43:10 -0700

    changed frequency of simulator

--- main.go
+++ main.go
@@ -68,7 +68,7 @@ func main() {
 	config := SimulatorConfig{
 		WebSocketURL: wsURL,
 		DeviceCount:  5,
-		LogInterval:  2 * time.Second,
+		LogInterval:  30 * time.Second,
 		Duration:     0, // Run indefinitely
 	}
 
